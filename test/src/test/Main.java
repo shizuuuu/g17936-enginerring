950bd7f3c94c1acad2aa3dc78167adc4b8dc5b27
erevest-cobalt <46865264+erevest-cobalt@users.noreply.github.com>
2020-05-15 19:36:59 +0900

    つくったよ！！！

---
+++ test/src/test/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author plutonium.aika
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("私の好きな食べ物はラーメンです．");
+		System.out.println("なぜかというと，ラーメンは出汁や脂身，香辛料など様々な旨味を使って作られており，複雑な風味を楽しめるからです．");
+	}
+
+}

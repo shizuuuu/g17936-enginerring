40d7ae7276d3100040f21e4b77b8b5ffe8a50651
erevest-cobalt <46865264+erevest-cobalt@users.noreply.github.com>
2020-05-15 19:54:15 +0900

    家系が好きです．

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -16,6 +16,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("私の好きな食べ物はラーメンです．");
 		System.out.println("なぜかというと，ラーメンは出汁や脂身，香辛料など様々な旨味を使って作られており，複雑な風味を楽しめるからです．");
+		System.out.println("ラーメンの中でも特に家系ラーメンが好きです．");
 	}
 
 }
